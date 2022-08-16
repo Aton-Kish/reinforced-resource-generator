e75d0ec46f8b494d36dd2e55b9fddb1215de8a07
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-16 13:35:35 +0000

    add material texture interface

--- src/assets/material/index.ts
+++ src/assets/material/index.ts
@@ -4,6 +4,12 @@ import Gold from './gold.png'
 import Iron from './iron.png'
 import Netherite from './netherite.png'
 
+export interface MaterialTexture {
+  namespace: string
+  name: string
+  src: string
+}
+
 export const MaterialCopperTexture = Copper
 export const MaterialIronTexture = Iron
 export const MaterialGoldTexture = Gold
