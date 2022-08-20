9160d23af13d20463bf7640236fc164a9a61f3f1
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-20 06:08:40 +0000

    refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import Generator from './components/Generator'
+import { useState } from 'react'
 
+import {
+  MaterialCopperTexture,
+  MaterialDiamondTexture,
+  MaterialGoldTexture,
+  MaterialIronTexture,
+  MaterialNetheriteTexture,
+} from './assets/material'
+import Generator from './components/Generator'
+import { MaterialContext } from './contexts'
+
+import type { MaterialTexture } from './assets/material'
 import type { FC } from 'react'
 
 const App: FC = () => {
+  const [materials, setMaterials] = useState<Record<string, MaterialTexture>>({
+    [MaterialCopperTexture.id]: MaterialCopperTexture,
+    [MaterialIronTexture.id]: MaterialIronTexture,
+    [MaterialGoldTexture.id]: MaterialGoldTexture,
+    [MaterialDiamondTexture.id]: MaterialDiamondTexture,
+    [MaterialNetheriteTexture.id]: MaterialNetheriteTexture,
+  })
+
   return (
     <div className='container mx-auto p-4'>
-      <Generator />
+      <MaterialContext.Provider value={{ materials, setMaterials }}>
+        <div className='flex flex-col gap-4'>
+          {Object.values(materials).map((material) => {
+            return <Generator key={material.id} material={material} />
+          })}
+        </div>
+      </MaterialContext.Provider>
     </div>
   )
 }
--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,42 +1,19 @@
-import { useState } from 'react'
-
-import {
-  MaterialCopperTexture,
-  MaterialDiamondTexture,
-  MaterialGoldTexture,
-  MaterialIronTexture,
-  MaterialNetheriteTexture,
-} from '../assets/material'
-import { MaterialContext } from '../contexts'
-
 import Input from './input/Input'
 import Output from './output/Output'
 
 import type { MaterialTexture } from '../assets/material'
 import type { FC } from 'react'
 
-const Generator: FC = () => {
-  const [materials, setMaterials] = useState<Record<string, MaterialTexture>>({
-    [MaterialCopperTexture.id]: MaterialCopperTexture,
-    [MaterialIronTexture.id]: MaterialIronTexture,
-    [MaterialGoldTexture.id]: MaterialGoldTexture,
-    [MaterialDiamondTexture.id]: MaterialDiamondTexture,
-    [MaterialNetheriteTexture.id]: MaterialNetheriteTexture,
-  })
+export interface GeneratorProps {
+  material: MaterialTexture
+}
 
+const Generator: FC<GeneratorProps> = ({ material }) => {
   return (
-    <MaterialContext.Provider value={{ materials, setMaterials }}>
-      <div>
-        {Object.values(materials).map((material) => {
-          return (
-            <div key={material.id} className='grid grid-cols-1 gap-4 md:grid-cols-[256px_1fr]'>
-              <Input material={material} />
-              <Output material={material} />
-            </div>
-          )
-        })}
-      </div>
-    </MaterialContext.Provider>
+    <div className='grid grid-cols-1 gap-4 rounded-lg border p-4 md:grid-cols-[256px_1fr]'>
+      <Input material={material} />
+      <Output material={material} />
+    </div>
   )
 }
 
