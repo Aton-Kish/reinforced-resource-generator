8b3a5acbe9fd3d2bffafdd906d1b1905c0607d35
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-21 04:58:30 +0000

    fix style

--- src/components/generator/GeneratorCard.tsx
+++ src/components/generator/GeneratorCard.tsx
@@ -14,17 +14,12 @@ const GeneratorCard = ({ material }: Props): JSX.Element => {
 
   return (
     <>
-      <div className={`rounded-lg border ${isOpen ? 'border-blue-500' : ''}`}>
-        <div
-          className={`flex cursor-pointer items-center gap-2 p-4 hover:bg-blue-50 ${
-            isOpen ? 'rounded-t-lg' : 'rounded-lg'
-          }`}
-          onClick={() => setIsOpen(!isOpen)}
-        >
+      <div className={`overflow-hidden rounded-lg border ${isOpen ? 'border-blue-500' : ''}`}>
+        <div className='flex cursor-pointer items-center gap-2 p-4 hover:bg-blue-50' onClick={() => setIsOpen(!isOpen)}>
           <img className='w-8' src={material.src} alt={material.name} title={material.name} />
           <h2 className='text-2xl'>{`${material.namespace}:${material.name}`}</h2>
         </div>
-        <div className={`overflow-hidden ${isOpen ? 'h-full' : 'h-0'}`}>
+        <div className={isOpen ? 'h-full' : 'h-0'}>
           <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-[256px_1fr]'>
             <Input material={material} />
             <Output material={material} />
