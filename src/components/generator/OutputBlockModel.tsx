024b3aa4930c2d10ab9f1ce51e636eb74fb832b9
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-28 03:01:46 +0000

    fix onclick event

--- src/components/generator/OutputBlockModel.tsx
+++ src/components/generator/OutputBlockModel.tsx
@@ -9,11 +9,16 @@ const OutputBlockModel = (): JSX.Element => {
 
   return (
     <div className='flex flex-col gap-1 overflow-hidden'>
-      <div className='inline-flex cursor-pointer items-center text-lg hover:text-blue-500'>
-        <svg className='h-4 w-4' fill='currentColor' viewBox='0 0 48 48'>
-          <path d={isOpen ? 'M44 10H4l20 20Z' : 'M10 44V4l20 20Z'} />
-        </svg>
-        <h3 onClick={() => setIsOpen(!isOpen)}>Block Model</h3>
+      <div>
+        <div
+          className='inline-flex cursor-pointer items-center text-lg hover:text-blue-500'
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg className='h-4 w-4' fill='currentColor' viewBox='0 0 48 48'>
+            <path d={isOpen ? 'M44 10H4l20 20Z' : 'M10 44V4l20 20Z'} />
+          </svg>
+          <h3>Block Model</h3>
+        </div>
       </div>
       <div className={`flex flex-col gap-1 ${isOpen ? 'h-full' : 'h-0'}`}>
         <OutputBlockModelChest />
