3c32fb172e89727dbea02b7802ea7baedac143ea
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-28 01:50:15 +0000

    add view box

--- src/components/configuration/MaterialsUploader.tsx
+++ src/components/configuration/MaterialsUploader.tsx
@@ -58,7 +58,7 @@ const MaterialsUploader = (): JSX.Element => {
       >
         <input {...getInputProps()} />
         <div className='flex flex-col text-center text-gray-500'>
-          <svg className='mx-auto h-12 w-12' fill='currentColor'>
+          <svg className='mx-auto h-12 w-12' fill='currentColor' viewBox='0 0 48 48'>
             <path d='M29.45 6v3H9v30h30V18.6h3V39q0 1.2-.9 2.1-.9.9-2.1.9H9q-1.2 0-2.1-.9Q6 40.2 6 39V9q0-1.2.9-2.1Q7.8 6 9 6ZM38 6v4.05h4.05v3H38v4.05h-3v-4.05h-4.05v-3H35V6ZM12 33.9h24l-7.2-9.6-6.35 8.35-4.7-6.2ZM9 9v30V9Z' />
           </svg>
           <p className='text-sm'>Upload material textures</p>
