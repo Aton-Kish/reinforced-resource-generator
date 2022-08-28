5e699f92e88911153dcc25b3f773379371d2e908
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-28 01:50:32 +0000

    add arrow icon

--- src/components/generator/OutputBlockState.tsx
+++ src/components/generator/OutputBlockState.tsx
@@ -9,9 +9,12 @@ const OutputBlockState = (): JSX.Element => {
 
   return (
     <div className='flex flex-col gap-1 overflow-hidden'>
-      <h3 className='cursor-pointer text-lg hover:text-blue-500' onClick={() => setIsOpen(!isOpen)}>
-        Block State
-      </h3>
+      <div className='inline-flex cursor-pointer items-center text-lg hover:text-blue-500'>
+        <svg className='h-4 w-4' fill='currentColor' viewBox='0 0 48 48'>
+          <path d={isOpen ? 'M44 10H4l20 20Z' : 'M10 44V4l20 20Z'} />
+        </svg>
+        <h3 onClick={() => setIsOpen(!isOpen)}>Block State</h3>
+      </div>
       <div className={`flex flex-col gap-1 ${isOpen ? 'h-full' : 'h-0'}`}>
         <OutputBlockStateChest />
         <OutputBlockStateShulker />
