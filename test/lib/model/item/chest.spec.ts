facf3bc1294f73abb314b305bae54829c55f2555
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-31 13:17:01 +0000

    add item model generate tests

--- test/lib/model/item/chest.spec.ts
+++ test/lib/model/item/chest.spec.ts
@@ -1,10 +1,43 @@
 import { MaterialCopperTexture, MaterialDiamondTexture } from '@/assets/material'
 import { ChestItemModelGenerator } from '@/lib/model/item'
 
+import CopperChestItemModel from './data/reinfchest/copper_chest.json'
+import DiamondChestItemModel from './data/reinfchest/diamond_chest.json'
+
 import type { ProjectConfig } from '@/lib/common'
+import type { ItemModel } from '@/lib/model/item'
 import type { MaterialTexture } from '@/lib/texture'
 
 describe('ChestItemModelGenerator', () => {
+  describe('generate()', () => {
+    const positiveCases: {
+      name: string
+      project: ProjectConfig
+      material: MaterialTexture
+      expected: ItemModel
+    }[] = [
+      {
+        name: 'positive case: reinfchest',
+        project: { namespace: 'reinfchest' },
+        material: MaterialCopperTexture,
+        expected: CopperChestItemModel,
+      },
+      {
+        name: 'positive case: reinfchest',
+        project: { namespace: 'reinfchest' },
+        material: MaterialDiamondTexture,
+        expected: DiamondChestItemModel,
+      },
+    ]
+
+    it.each(positiveCases)('$name', ({ project, material, expected }) => {
+      const generator = new ChestItemModelGenerator(project, material)
+
+      const actual = generator.generate()
+      expect(actual).toStrictEqual(expected)
+    })
+  })
+
   describe('path()', () => {
     const positiveCases: {
       name: string
--- test/lib/model/item/shulker.spec.ts
+++ test/lib/model/item/shulker.spec.ts
@@ -2,10 +2,54 @@ import { MaterialCopperTexture, MaterialDiamondTexture, MaterialNetheriteTexture
 import { ShulkerType } from '@/lib/common'
 import { ShulkerItemModelGenerator } from '@/lib/model/item'
 
+import CopperShulkerBoxItemModel from './data/reinfshulker/copper_shulker_box.json'
+import GrayDiamondShulkerBoxItemModel from './data/reinfshulker/gray_diamond_shulker_box.json'
+import LightGrayNetheriteShulkerBoxItemModel from './data/reinfshulker/light_gray_netherite_shulker_box.json'
+
 import type { ProjectConfig } from '@/lib/common'
+import type { ItemModel } from '@/lib/model/item'
 import type { MaterialTexture } from '@/lib/texture'
 
 describe('ShulkerItemModelGenerator', () => {
+  describe('generate()', () => {
+    const positiveCases: {
+      name: string
+      project: ProjectConfig
+      material: MaterialTexture
+      type: ShulkerType
+      expected: ItemModel
+    }[] = [
+      {
+        name: 'positive case: reinfshulker:copper_shulker_box',
+        project: { namespace: 'reinfshulker' },
+        material: MaterialCopperTexture,
+        type: ShulkerType.Default,
+        expected: CopperShulkerBoxItemModel,
+      },
+      {
+        name: 'positive case: reinfshulker:gray_diamond_shulker_box',
+        project: { namespace: 'reinfshulker' },
+        material: MaterialDiamondTexture,
+        type: ShulkerType.Gray,
+        expected: GrayDiamondShulkerBoxItemModel,
+      },
+      {
+        name: 'positive case: reinfshulker:light_gray_netherite_shulker_box',
+        project: { namespace: 'reinfshulker' },
+        material: MaterialNetheriteTexture,
+        type: ShulkerType.LightGray,
+        expected: LightGrayNetheriteShulkerBoxItemModel,
+      },
+    ]
+
+    it.each(positiveCases)('$name', ({ project, material, type, expected }) => {
+      const generator = new ShulkerItemModelGenerator(project, material)
+
+      const actual = generator.generate(type)
+      expect(actual).toStrictEqual(expected)
+    })
+  })
+
   describe('path()', () => {
     const positiveCases: {
       name: string
