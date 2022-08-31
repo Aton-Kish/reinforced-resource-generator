bb12b6cbadafafc578966c68f36834aadcb5878d
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-31 12:13:13 +0000

    add language path tests

---
+++ test/lib/language/shulker.spec.ts
@@ -0,0 +1,36 @@
+import { MaterialCopperTexture, MaterialDiamondTexture } from '@/assets/material'
+import { ShulkerLanguageGenerator } from '@/lib/language'
+
+import type { ProjectConfig } from '@/lib/common'
+import type { MaterialTexture } from '@/lib/texture'
+
+describe('ShulkerLanguageGenerator', () => {
+  describe('path()', () => {
+    const positiveCases: {
+      name: string
+      project: ProjectConfig
+      material: MaterialTexture
+      expected: string
+    }[] = [
+      {
+        name: 'positive case: reinfshulker',
+        project: { namespace: 'reinfshulker' },
+        material: MaterialCopperTexture,
+        expected: 'assets/reinfshulker/lang/en_us.json',
+      },
+      {
+        name: 'positive case: reinfstorage',
+        project: { namespace: 'reinfstorage' },
+        material: MaterialDiamondTexture,
+        expected: 'assets/reinfstorage/lang/en_us.json',
+      },
+    ]
+
+    it.each(positiveCases)('$name', ({ project, material, expected }) => {
+      const generator = new ShulkerLanguageGenerator(project, material)
+
+      const actual = generator.path()
+      expect(actual).toBe(expected)
+    })
+  })
+})
