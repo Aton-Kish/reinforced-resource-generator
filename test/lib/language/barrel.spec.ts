72899354f934670c8165f38e87b03cdb35189acf
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-09-03 06:52:20 +0000

    add merged language test

--- test/lib/language/barrel.spec.ts
+++ test/lib/language/barrel.spec.ts
@@ -1,10 +1,72 @@
-import { MaterialCopperTexture, MaterialDiamondTexture } from '@/assets/material'
+import merge from 'ts-deepmerge'
+
+import {
+  MaterialCopperTexture,
+  MaterialDiamondTexture,
+  MaterialGoldTexture,
+  MaterialIronTexture,
+  MaterialNetheriteTexture,
+} from '@/assets/material'
 import { BarrelLanguageGenerator } from '@/lib/language'
 
+import CopperBarrelLanguage from './data/reinfbarrel/copper_en_us.json'
+import DiamondBarrelLanguage from './data/reinfbarrel/diamond_en_us.json'
+import BarrelLanguage from './data/reinfbarrel/en_us.json'
+
 import type { ProjectConfig } from '@/lib/common'
+import type { Language } from '@/lib/language'
 import type { MaterialTexture } from '@/lib/texture'
 
 describe('BarrelLanguageGenerator', () => {
+  describe('generate()', () => {
+    const positiveCases: {
+      name: string
+      project: ProjectConfig
+      material: MaterialTexture
+      expected: Language
+    }[] = [
+      {
+        name: 'positive case: reinfbarrel:copper_barrel',
+        project: { namespace: 'reinfbarrel' },
+        material: MaterialCopperTexture,
+        expected: CopperBarrelLanguage,
+      },
+      {
+        name: 'positive case: reinfbarrel:diamond_barrel',
+        project: { namespace: 'reinfbarrel' },
+        material: MaterialDiamondTexture,
+        expected: DiamondBarrelLanguage,
+      },
+    ]
+
+    it('positive case: reinfbarrel', () => {
+      const project: ProjectConfig = { namespace: 'reinfbarrel' }
+
+      const copperGenerator = new BarrelLanguageGenerator(project, MaterialCopperTexture)
+      const ironGenerator = new BarrelLanguageGenerator(project, MaterialIronTexture)
+      const goldGenerator = new BarrelLanguageGenerator(project, MaterialGoldTexture)
+      const diamondGenerator = new BarrelLanguageGenerator(project, MaterialDiamondTexture)
+      const netheriteGenerator = new BarrelLanguageGenerator(project, MaterialNetheriteTexture)
+
+      const actual = merge(
+        copperGenerator.generate(),
+        ironGenerator.generate(),
+        goldGenerator.generate(),
+        diamondGenerator.generate(),
+        netheriteGenerator.generate(),
+      )
+      const expected: Language = BarrelLanguage
+      expect(actual).toStrictEqual(expected)
+    })
+
+    it.each(positiveCases)('$name', ({ project, material, expected }) => {
+      const generator = new BarrelLanguageGenerator(project, material)
+
+      const actual = generator.generate()
+      expect(actual).toStrictEqual(expected)
+    })
+  })
+
   describe('path()', () => {
     const positiveCases: {
       name: string
--- test/lib/language/chest.spec.ts
+++ test/lib/language/chest.spec.ts
@@ -1,10 +1,72 @@
-import { MaterialCopperTexture, MaterialDiamondTexture } from '@/assets/material'
+import merge from 'ts-deepmerge'
+
+import {
+  MaterialCopperTexture,
+  MaterialDiamondTexture,
+  MaterialGoldTexture,
+  MaterialIronTexture,
+  MaterialNetheriteTexture,
+} from '@/assets/material'
 import { ChestLanguageGenerator } from '@/lib/language'
 
+import CopperChestLanguage from './data/reinfchest/copper_en_us.json'
+import DiamondChestLanguage from './data/reinfchest/diamond_en_us.json'
+import ChestLanguage from './data/reinfchest/en_us.json'
+
 import type { ProjectConfig } from '@/lib/common'
+import type { Language } from '@/lib/language'
 import type { MaterialTexture } from '@/lib/texture'
 
 describe('ChestLanguageGenerator', () => {
+  describe('generate()', () => {
+    const positiveCases: {
+      name: string
+      project: ProjectConfig
+      material: MaterialTexture
+      expected: Language
+    }[] = [
+      {
+        name: 'positive case: reinfchest:copper_chest',
+        project: { namespace: 'reinfchest' },
+        material: MaterialCopperTexture,
+        expected: CopperChestLanguage,
+      },
+      {
+        name: 'positive case: reinfchest:diamond_chest',
+        project: { namespace: 'reinfchest' },
+        material: MaterialDiamondTexture,
+        expected: DiamondChestLanguage,
+      },
+    ]
+
+    it('positive case: reinfchest', () => {
+      const project: ProjectConfig = { namespace: 'reinfchest' }
+
+      const copperGenerator = new ChestLanguageGenerator(project, MaterialCopperTexture)
+      const ironGenerator = new ChestLanguageGenerator(project, MaterialIronTexture)
+      const goldGenerator = new ChestLanguageGenerator(project, MaterialGoldTexture)
+      const diamondGenerator = new ChestLanguageGenerator(project, MaterialDiamondTexture)
+      const netheriteGenerator = new ChestLanguageGenerator(project, MaterialNetheriteTexture)
+
+      const actual = merge(
+        copperGenerator.generate(),
+        ironGenerator.generate(),
+        goldGenerator.generate(),
+        diamondGenerator.generate(),
+        netheriteGenerator.generate(),
+      )
+      const expected: Language = ChestLanguage
+      expect(actual).toStrictEqual(expected)
+    })
+
+    it.each(positiveCases)('$name', ({ project, material, expected }) => {
+      const generator = new ChestLanguageGenerator(project, material)
+
+      const actual = generator.generate()
+      expect(actual).toStrictEqual(expected)
+    })
+  })
+
   describe('path()', () => {
     const positiveCases: {
       name: string
