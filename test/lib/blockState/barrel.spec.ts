80859bf9264ee1be02af66eda363117d06293283
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-09-03 09:24:25 +0000

    add block state zipper tests

--- test/lib/blockState/barrel.spec.ts
+++ test/lib/blockState/barrel.spec.ts
@@ -1,9 +1,12 @@
+import JSZip from 'jszip'
+
 import { MaterialCopperTexture, MaterialDiamondTexture } from '@/assets/material'
 import { BarrelBlockStateGenerator } from '@/lib/blockState'
 
 import CopperBarrelBlockState from './data/reinfbarrel/copper_barrel.json'
 import DiamondBarrelBlockState from './data/reinfbarrel/diamond_barrel.json'
 
+import type { Constructable } from '#/types'
 import type { BlockState } from '@/lib/blockState'
 import type { ProjectConfig } from '@/lib/common'
 import type { MaterialTexture } from '@/lib/texture'
@@ -64,6 +67,58 @@ describe('BarrelBlockStateGenerator', () => {
 
       const actual = generator.path()
       expect(actual).toBe(expected)
+    })
+  })
+
+  describe('async zip()', () => {
+    const positiveCases: {
+      name: string
+      project: ProjectConfig
+      material: MaterialTexture
+      expected: {
+        path: string
+        data: BlockState
+      }
+    }[] = [
+      {
+        name: 'positive case: reinfbarrel:copper_barrel',
+        project: { namespace: 'reinfbarrel' },
+        material: MaterialCopperTexture,
+        expected: {
+          path: 'assets/reinfbarrel/blockstates/copper_barrel.json',
+          data: CopperBarrelBlockState,
+        },
+      },
+      {
+        name: 'positive case: reinfbarrel:diamond_barrel',
+        project: { namespace: 'reinfbarrel' },
+        material: MaterialDiamondTexture,
+        expected: {
+          path: 'assets/reinfbarrel/blockstates/diamond_barrel.json',
+          data: DiamondBarrelBlockState,
+        },
+      },
+    ]
+
+    it.each(positiveCases)('$name', async ({ project, material, expected }) => {
+      const generator = new BarrelBlockStateGenerator(project, material)
+
+      const zip = new JSZip()
+
+      const actual = await generator.zip(zip)
+      expect(Object.keys(actual.files)).toContain(expected.path)
+      expect(JSON.parse(await actual.file(expected.path)!.async('string'))).toStrictEqual(expected.data)
+    })
+
+    it('negative case', async () => {
+      const project: ProjectConfig = { namespace: 'reinfchest' }
+      const material = MaterialCopperTexture
+      const generator = new BarrelBlockStateGenerator(project, material)
+
+      const zip = await generator.zip(new JSZip())
+      const expected = new Error(`file already exists: ${generator.path()}`)
+      expect(async () => await generator.zip(zip)).rejects.toThrow(expected.constructor as Constructable<Error>)
+      expect(async () => await generator.zip(zip)).rejects.toThrow(expected)
     })
   })
 })
--- test/lib/blockState/shulker.spec.ts
+++ test/lib/blockState/shulker.spec.ts
@@ -1,3 +1,5 @@
+import JSZip from 'jszip'
+
 import { MaterialCopperTexture, MaterialDiamondTexture, MaterialNetheriteTexture } from '@/assets/material'
 import { ShulkerBlockStateGenerator } from '@/lib/blockState'
 import { ShulkerType } from '@/lib/common'
@@ -6,6 +8,7 @@ import CopperShulkerBoxBlockState from './data/reinfshulker/copper_shulker_box.j
 import GrayDiamondShulkerBoxBlockState from './data/reinfshulker/gray_diamond_shulker_box.json'
 import LightGrayNetheriteShulkerBoxBlockState from './data/reinfshulker/light_gray_netherite_shulker_box.json'
 
+import type { Constructable } from '#/types'
 import type { BlockState } from '@/lib/blockState'
 import type { ProjectConfig } from '@/lib/common'
 import type { MaterialTexture } from '@/lib/texture'
@@ -86,6 +89,72 @@ describe('ShulkerBlockStateGenerator', () => {
 
       const actual = generator.path(type)
       expect(actual).toBe(expected)
+    })
+  })
+
+  describe('async zip()', () => {
+    const positiveCases: {
+      name: string
+      project: ProjectConfig
+      material: MaterialTexture
+      type: ShulkerType
+      expected: {
+        path: string
+        data: BlockState
+      }
+    }[] = [
+      {
+        name: 'positive case: reinfshulker:copper_shulker_box',
+        project: { namespace: 'reinfshulker' },
+        material: MaterialCopperTexture,
+        type: ShulkerType.Default,
+        expected: {
+          path: 'assets/reinfshulker/blockstates/copper_shulker_box.json',
+          data: CopperShulkerBoxBlockState,
+        },
+      },
+      {
+        name: 'positive case: reinfshulker:gray_diamond_shulker_box',
+        project: { namespace: 'reinfshulker' },
+        material: MaterialDiamondTexture,
+        type: ShulkerType.Gray,
+        expected: {
+          path: 'assets/reinfshulker/blockstates/gray_diamond_shulker_box.json',
+          data: GrayDiamondShulkerBoxBlockState,
+        },
+      },
+      {
+        name: 'positive case: reinfshulker:light_gray_netherite_shulker_box',
+        project: { namespace: 'reinfshulker' },
+        material: MaterialNetheriteTexture,
+        type: ShulkerType.LightGray,
+        expected: {
+          path: 'assets/reinfshulker/blockstates/light_gray_netherite_shulker_box.json',
+          data: LightGrayNetheriteShulkerBoxBlockState,
+        },
+      },
+    ]
+
+    it.each(positiveCases)('$name', async ({ project, material, type, expected }) => {
+      const generator = new ShulkerBlockStateGenerator(project, material)
+
+      const zip = new JSZip()
+
+      const actual = await generator.zip(zip, type)
+      expect(Object.keys(actual.files)).toContain(expected.path)
+      expect(JSON.parse(await actual.file(expected.path)!.async('string'))).toStrictEqual(expected.data)
+    })
+
+    it('negative case', async () => {
+      const project: ProjectConfig = { namespace: 'reinfshulker' }
+      const material = MaterialCopperTexture
+      const type = ShulkerType.Default
+      const generator = new ShulkerBlockStateGenerator(project, material)
+
+      const zip = await generator.zip(new JSZip(), type)
+      const expected = new Error(`file already exists: ${generator.path(type)}`)
+      expect(async () => await generator.zip(zip, type)).rejects.toThrow(expected.constructor as Constructable<Error>)
+      expect(async () => await generator.zip(zip, type)).rejects.toThrow(expected)
     })
   })
 })
